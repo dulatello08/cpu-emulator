a4655c591518d2cb124b3245ac702c2235b18a61
Dulat <dulatello08@gmail.com>
2023-01-10 20:59:57 +0000

    I tried once again...

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include "main.h"
 #include <endian.h>
@@ -20,6 +22,7 @@ int main(int argc, char *argv[]) {
                 break;
             case 'm':
                 flash_file = optarg;
+                break;
             case '?':
                 if (optopt == 'i' || optopt == 'm') {
                     fprintf(stderr, "Option -%c requires an argument.\n", optopt);
@@ -45,18 +48,19 @@ int main(int argc, char *argv[]) {
     if (input_file != NULL) {
         printf("Input file: %s\n", input_file);
 
-        FILE *fp = fopen(input_file, "rb");
-        if (fp == NULL) {
+        FILE *fpi = fopen(input_file, "rb");
+        FILE *fpf = fopen(flash_file, "rb");
+        if (fpi == NULL) {
             // Failed to open the file, return an error
             printf("Failed to open input file");
             return -1;
         }
 
         // Move the file pointer to the end of the file
-        fseek(fp, 0, SEEK_END);
+        fseek(fpi, 0, SEEK_END);
 
         // Get the size of the file in bytes
-        long size = ftell(fp);
+        long size = ftell(fpi);
 
         // Calculate the number of 16-bit words in the file
         u_int64_t num_words = size / 2;
@@ -65,26 +69,49 @@ int main(int argc, char *argv[]) {
             // File has the wrong size, return an error
             printf("File does not contain 255 16-bit words\n");
             printf("It contains %lu of 16-bit words", num_words);
-            fclose(fp);
+            fclose(fpi);
+            return -1;
+        }
+
+         if (fpf == NULL) {
+            // Failed to open the file, return an error
+            printf("Failed to open input file");
+            return -1;
+        }
+
+        // Move the file pointer to the end of the file
+        fseek(fpf, 0, SEEK_END);
+
+        // Get the size of the file in bytes
+        long sizeF = ftell(fpi);
+
+        // Calculate the number of 16-bit words in the file
+        u_int64_t num_wordsF = sizeF / 2;
+
+        if (num_wordsF != 255) {
+            // File has the wrong size, return an error
+            printf("File does not contain 255 16-bit words\n");
+            printf("It contains %lu of 16-bit words", num_wordsF);
+            fclose(fpi);
             return -1;
         }
         uint16_t *program_memory;
         program_memory = malloc(255*sizeof(uint16_t));
-        fseek(fp, 0, SEEK_SET);
+        fseek(fpi, 0, SEEK_SET);
         // Read the words from the file
-        size_t num_read = fread(program_memory, sizeof(uint16_t), 255, fp);
+        size_t num_read = fread(program_memory, sizeof(uint16_t), 255, fpi);
         if (num_read != 255) {
             // Failed to read the expected number of words, return an error
             free(program_memory);
-            fclose(fp);
+            fclose(fpi);
             return -1;
         }
         *program_memory = ntohs(*program_memory);
         *program_memory = htobe16(*program_memory);
-
-        start(program_memory);
+        uint8_t hi;
+        start(program_memory, &hi);
         // File has the correct size, close the file and return success
-        fclose(fp);
+        fclose(fpi);
     }
 
     return 0;
