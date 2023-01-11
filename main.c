43ff0aa1697b3511a481aafc28636ff409075980
Dulat <dulatello08@gmail.com>
2023-01-11 17:39:40 +0000

    It has to work now

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
     int help_flag = 0;
     char *input_file = NULL;
     char *flash_file = NULL;
-    while ((c = getopt(argc, argv, "him:")) != -1) {
+    while ((c = getopt(argc, argv, "hm:i:")) != -1) {
         switch (c) {
             case 'h':
                 help_flag = 1;
@@ -46,13 +46,11 @@ int main(int argc, char *argv[]) {
     }
 
     if (input_file != NULL) {
-        printf("Input file: %s\n", input_file);
-
+        printf("Input flash file: %s\n", input_file);
         FILE *fpi = fopen(input_file, "rb");
-        FILE *fpf = fopen(flash_file, "rb");
         if (fpi == NULL) {
             // Failed to open the file, return an error
-            printf("Failed to open input file");
+            printf("Failed to open input program");
             return -1;
         }
 
@@ -68,33 +66,48 @@ int main(int argc, char *argv[]) {
         if (num_words != 255) {
             // File has the wrong size, return an error
             printf("File does not contain 255 16-bit words\n");
-            printf("It contains %lu of 16-bit words", num_words);
+            printf("It contains%lu of 16-bit words", num_words);
             fclose(fpi);
             return -1;
         }
+        uint8_t *flash_memory = NULL;
+        if(flash_file!=NULL) {
+            FILE *fpf = fopen(flash_file, "rb");
+            if (fpf == NULL) {
+                // Failed to open the file, return an error
+                printf("Failed to open input flash\n");
+                return -1;
+            }
 
-         if (fpf == NULL) {
-            // Failed to open the file, return an error
-            printf("Failed to open input file");
-            return -1;
+            // Move the file pointer to the end of the file
+            fseek(fpf, 0, SEEK_END);
+
+            // Get the size of the file in bytes
+            long sizeF = ftell(fpi);
+
+            // Calculate the number of 16-bit words in the file
+            u_int64_t num_wordsF = sizeF;
+
+            if (num_wordsF != 255) {
+                // File has the wrong size, return an error
+                printf("File does not contain 255 16-bit words\n");
+                printf("It contains %lu of 16-bit words", num_wordsF);
+                fclose(fpi);
+                return -1;
+            }
+            
+            flash_memory = malloc(255*sizeof(uint16_t));
+            fseek(fpi, 0, SEEK_SET);
+            // Read the words from the file
+            size_t num_read = fread(flash_memory, sizeof(uint16_t), 255, fpi);
+            if (num_read != 255) {
+                // Failed to read the expected number of words, return an error
+                free(flash_memory);
+                fclose(fpi);
+                return -1;
+            }
         }
-
-        // Move the file pointer to the end of the file
-        fseek(fpf, 0, SEEK_END);
-
-        // Get the size of the file in bytes
-        long sizeF = ftell(fpi);
-
-        // Calculate the number of 16-bit words in the file
-        u_int64_t num_wordsF = sizeF / 2;
-
-        if (num_wordsF != 255) {
-            // File has the wrong size, return an error
-            printf("File does not contain 255 16-bit words\n");
-            printf("It contains %lu of 16-bit words", num_wordsF);
-            fclose(fpi);
-            return -1;
-        }
+        
         uint16_t *program_memory;
         program_memory = malloc(255*sizeof(uint16_t));
         fseek(fpi, 0, SEEK_SET);
@@ -108,8 +121,7 @@ int main(int argc, char *argv[]) {
         }
         *program_memory = ntohs(*program_memory);
         *program_memory = htobe16(*program_memory);
-        uint8_t hi;
-        start(program_memory, &hi);
+        start(program_memory, flash_memory);
         // File has the correct size, close the file and return success
         fclose(fpi);
     }
