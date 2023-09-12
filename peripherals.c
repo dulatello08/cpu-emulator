ed2542ff2bee89994e2b76c60214be9df6880fef
Dulat S <dulatello08@gmail.com>
2023-09-11 21:41:34 -0700

    upcoming keyboard work, stable display, 91 byte hello world program.
    idk how steve wozniak fit entire OS (arguably, WozMon) into 255 bytes of machine code...

--- peripherals.c
+++ peripherals.c
@@ -17,19 +17,24 @@ void print_display(char display[LCD_WIDTH][LCD_HEIGHT]) {
     // Print the display to the console
     for (int j = 0; j < LCD_HEIGHT; j++) {
         for (int i = 0; i < LCD_WIDTH; i++) {
-            printf("%d", display[i][j]);
+            char character = display[i][j];
+            // Replace null character with '*'
+            if (character == '\0') {
+                character = '*';
+            }
+            printf("%c", character);
         }
         printf("\n");
     }
 }
 
-void write_to_display(char display[LCD_WIDTH][LCD_HEIGHT], char data) {
+void write_to_display(char display[LCD_WIDTH][LCD_HEIGHT], uint8_t data) {
     static int x = 0;
     static int y = 0;
 
     if (data >= 0x20 && data <= 0x7e) {
         // Printable ASCII character
-        display[x][y] = data;
+        display[x][y] = (char) data;
         x++;
         if (x == LCD_WIDTH) {
             // Move to next line if we reach the end of the row
