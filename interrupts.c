d8e2dbff6dbb21a3146e07112f7dc825fef78b7e
Dulat <dulatello08@gmail.com>
2023-09-29 20:18:42 +0000

    interrupt queue and stuff

--- interrupts.c
+++ interrupts.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdint.h>
+#include <stdlib.h>
 
 
 void add_interrupt_vector(InterruptVectors table[INTERRUPT_TABLE_SIZE], uint8_t index, uint8_t source, uint16_t handler) {
@@ -19,8 +21,29 @@ uint16_t get_interrupt_handler(const InterruptVectors table[INTERRUPT_TABLE_SIZE
 }
 
 void push_interrupt(InterruptQueue* queue, uint8_t source) {
+    queue->size++;
+    queue->sources = realloc(queue->sources, queue->size * sizeof(uint8_t));
+    
+    for (int i = queue->size - 1; i > 0; i--) {
+        queue->sources[i] = queue->sources[i - 1];
+    }
+    
+    queue->sources[0] = source;
 }
 
 uint8_t pop_interrupt(InterruptQueue* queue) {
+    if (queue->size == 0) {
+        return 0;
+    }
 
+    uint8_t source = queue->sources[0];
+
+    for (int i = 1; i < queue->size; i++) {
+        queue->sources[i - 1] = queue->sources[i];
+    }
+
+    queue->size--;
+    queue->sources = realloc(queue->sources, queue->size * sizeof(uint8_t));
+
+    return source;
 }
