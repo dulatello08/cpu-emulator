e96e04523e9da50627a3d9850d45cdc7b68f37e2
Dulat S <dulatello08@gmail.com>
2023-09-28 21:45:19 -0700

    refactor i queue

--- emulator.c
+++ emulator.c
@@ -14,12 +14,15 @@ int start(CPUState *state, size_t program_size, size_t flash_size, const uint8_t
     state->v_flag = false;
     state->z_flag = false;
     state->memory = memory;
+
+    state->i_queue.sources = NULL;
+    state->i_queue.top = SENTINEL_VALUE;
     //printf("From emulator, register pointer: %p\n", (void *) state->reg);
     memcpy(state->memory, program_memory, program_size);
 
     setupMmap(state, program_size);
     state->in_subroutine = &(state->memory[state->mm.flagsBlock.startAddress]);
-    printf("Flash size: %zu", flash_size);
+    printf("Flash size: %zu",  flash_size);
     if (flash_size > BLOCK_SIZE) {
         memcpy(&(state->memory[state->mm.currentFlashBlock.startAddress]), flash_memory[0], 4096);
     } else {
@@ -29,6 +32,7 @@ int start(CPUState *state, size_t program_size, size_t flash_size, const uint8_t
     printf("Starting emulator\n");
     bool exitCode = false;
     while (*(state->pc) + 1 < UINT16_MAX && !exitCode) {
+        if (state->i_queue.top == SENTINEL_VALUE || state->i_queue.top == 0)
         //usleep(500000);
         exitCode = execute_instruction(state);
     }
