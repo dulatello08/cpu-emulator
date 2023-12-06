d95f71286403da129ce0d76655ad2402a4c3e77d
Dulat <dulatello08@gmail.com>
2023-12-05 21:46:33 -0800

    Refactor keyboard event handling
    Refactored the keyboard.c to handle key code events for user inputs. This approach provides reliable real-time user input capture.

--- keyboard/keyboard.c
+++ keyboard/keyboard.c
@@ -1,3 +1,24 @@
-//
-// Created by Dulat S on 8/27/23.
-//
+#include<stdio.h>
+#include<fcntl.h>
+#include<linux/input.h>
+
+int main() {
+    // Change this to your actual keyboard's event file path.
+    const char *dev = "/dev/input/event5";
+    struct input_event ev;
+
+    int fd = open(dev, O_RDONLY);
+    if (fd == -1) {
+        perror("Cannot open input device");
+        return 1;
+    }
+
+    while (read(fd, &ev, sizeof(ev)) == sizeof(ev)) {
+        if (ev.type == EV_KEY) {
+            printf("Key event: code %d value %d\n", ev.code, ev.value);
+        }
+    }
+
+    close(fd);
+    return 0;
+}
