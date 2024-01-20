daf7427f01779807e877732c855b8099ea7302a8
Dulat S <dulatello08@gmail.com>
2024-01-19 22:00:17 -0800

    starting work on modularized adapters for IPC

---
+++ ipc/main.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Dulat S on 1/19/24.
+//
+
+#include "main.h"
+
+// modules as function pointers to main function
+
---
+++ ipc/main.h
@@ -0,0 +1,10 @@
+//
+// Created by Dulat S on 1/19/24.
+//
+
+#ifndef IPC_MAIN_H
+#define IPC_MAIN_H
+
+#include <stdio.h>
+
+#endif //IPC_MAIN_H
